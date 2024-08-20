d82e39c48450ef1bd6833b74f278b157c7cd961a
Hassan Abbud <hassanabbud@hotmail.com>
2024-08-20 17:38:27 -0600

    Adding JwtAuthenticationFilter fro login

--- src/main/java/com/hassan/springboot/app/crudjpa/springboot_crud/repositories/UserRepository.java
+++ src/main/java/com/hassan/springboot/app/crudjpa/springboot_crud/repositories/UserRepository.java
@@ -1,5 +1,6 @@
 package com.hassan.springboot.app.crudjpa.springboot_crud.repositories;
 
+import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 
 import com.hassan.springboot.app.crudjpa.springboot_crud.entities.User;
@@ -7,5 +8,7 @@ import com.hassan.springboot.app.crudjpa.springboot_crud.entities.User;
 public interface UserRepository extends CrudRepository<User, Long>{
 
     boolean existsByUsername(String username);
-    
+
+
+    Optional<User> findByUsername(String username);
 }
