64b305081b55b2be25a79650cf6ef08108e2b767
Hassan Abbud <hassanabbud@hotmail.com>
2024-08-21 13:54:19 -0600

    Comment overlapped hasRole() with annotations PreAuthorize(). Checking authorization 2 times

--- src/main/java/com/hassan/springboot/app/crudjpa/springboot_crud/security/SpringSecurity.java
+++ src/main/java/com/hassan/springboot/app/crudjpa/springboot_crud/security/SpringSecurity.java
@@ -38,11 +38,11 @@ public class SpringSecurity {
         return http.authorizeHttpRequests( (authz) -> authz
         .requestMatchers(HttpMethod.GET, "/api/users").permitAll()
         .requestMatchers(HttpMethod.POST, "/api/users/register").permitAll()
-        .requestMatchers(HttpMethod.POST, "/api/users").hasRole("ADMIN") //ROLE_ added automatically
-        .requestMatchers(HttpMethod.GET, "/api/products", "/api/products/{id}").hasAnyRole("ADMIN", "USER")
-        .requestMatchers(HttpMethod.POST, "/api/products").hasRole("ADMIN")
-        .requestMatchers(HttpMethod.PUT, "/api/products/{id}").hasRole("ADMIN")
-        .requestMatchers(HttpMethod.DELETE, "/api/products/{id}").hasRole("ADMIN")
+        // .requestMatchers(HttpMethod.POST, "/api/users").hasRole("ADMIN") //ROLE_ added automatically
+        // .requestMatchers(HttpMethod.GET, "/api/products", "/api/products/{id}").hasAnyRole("ADMIN", "USER")
+        // .requestMatchers(HttpMethod.POST, "/api/products").hasRole("ADMIN")
+        // .requestMatchers(HttpMethod.PUT, "/api/products/{id}").hasRole("ADMIN")
+        // .requestMatchers(HttpMethod.DELETE, "/api/products/{id}").hasRole("ADMIN")
             .anyRequest().authenticated())
         .addFilter(new JwtAuthenticationFilter(authenticationManager()))
         .addFilter(new JwtValidationFilter(authenticationManager()))
